395598ca2bc2c23002ea17b420983eb83c83192f
hayrapetyanlusine <lusinehayrapetyan0220@gmail.com>
2023-05-03 21:44:02 +0400

    create footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import logo from "../../images/logo.png"
+import { FacebookSvg, InstagramSvg, LinkedinSvg, TwitterSvg } from '../../svg/svg'
+import "./Footer.scss"
 
 export const Footer = () => {
   return (
-    <div style={{height: "200px", backgroundColor: 'rgba(0, 117, 163, .75)'}} className='container'>
-        Footer
-    </div>
+    <footer>
+      <div className='container'>
+
+        <div className='logo'>
+          <img src={logo} alt='logo' />
+        </div>
+
+        <div className='footer-content'>
+          <p>12345 Ocean Ave. <br /> Long Branch, NJ</p>
+          <hr />
+          <p>732-555-1234</p>
+          <div className='legal-content'>
+            <p>Privacy Policy</p>
+            <p>Terms & Conditions</p>
+          </div>
+          <p><span>Website by Larsen Web Designs.com</span></p>
+        </div>
+
+        <div className='social-block'>
+          <span> <FacebookSvg /> </span>
+          <span> <InstagramSvg /> </span>
+          <span> <LinkedinSvg /> </span>
+          <span> <TwitterSvg /> </span>
+        </div>
+
+      </div>
+    </footer>
   )
 }
